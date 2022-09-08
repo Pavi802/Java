e627fcf0ba2d753d1a4d0545a8220a01cab71544
Pavithra Natarajan <109599417+Pavi802@users.noreply.github.com>
2022-09-08 07:55:41 -0600

    Create 3(1).java

---
+++ 3(1).java
@@ -0,0 +1,19 @@
+//Write a java program to check whether a Number is Even or Odd
+import java.io.*;
+class even
+{
+    public static void main(String args[])throws IOException
+    {
+        DataInputStream d=new DataInputStream(System.in);
+        System.out.print("Enter the Number=");
+        int a=Integer.parseInt(d.readLine());
+        if(a%2==0)
+        {
+            System.out.println("The given value is Even");
+        }
+        else
+        {
+            System.out.println("The given value is Odd");
+        }
+    }
+}
