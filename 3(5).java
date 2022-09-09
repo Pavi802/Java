421d634cc68e0cefb5e5a68b10cee805bcd90715
Pavithra Natarajan <109599417+Pavi802@users.noreply.github.com>
2022-09-09 10:48:15 +0530

    Create 3(5).java

---
+++ 3(5).java
@@ -0,0 +1,24 @@
+//write a java program to check the number is positive or negative
+
+import java.io.*;
+class pos
+{
+    public static void main(String args[])throws IOException
+    {
+        DataInputStream d=new DataInputStream(System.in);
+        System.out.print("Enter the number=");
+        double n=Double.parseDouble(d.readLine());
+        if(n>0.0)
+        {
+            System.out.println("The number is Positive");
+        }
+        else if(n<0.0)
+        {
+            System.out.println("The number is Negative");
+        }
+        else
+        {
+            System.out.println("The number is 0");
+        }
+    }
+}
