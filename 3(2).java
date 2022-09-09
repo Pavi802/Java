e7a68f8b1f46a7fee6c2fd86d364476e3cd18834
Pavithra Natarajan <109599417+Pavi802@users.noreply.github.com>
2022-09-09 10:08:28 +0530

    Create 3(2).java

---
+++ 3(2).java
@@ -0,0 +1,21 @@
+//Write a java program to check whether an alphabet is vowel or consonant
+
+import java.io.*;
+class vowel
+{
+    public static void main(String args[])throws  IOException
+    {
+        DataInputStream d= new DataInputStream(System.in);
+        System.out.print("The Character is=");
+        char c=d.readLine().charAt(0);
+        if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u')
+        {
+            System.out.println("The character is Vowel");
+        }
+        else
+        {
+            System.out.println("The Character is Consonant");
+        }
+        
+    }
+}
