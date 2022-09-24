a26dacac764c98c36c5b5237c26cb9849ec4fe02
Pavithra Natarajan <109599417+Pavi802@users.noreply.github.com>
2022-09-24 07:17:21 -0600

    Create Day_15_CurrentDate.java

---
+++ Day_15_CurrentDate.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.time.LocalDateTime;
+class Main {
+          public static void main (String[] args)throws IOException 
+          {
+               DataInputStream d=new DataInputStream(System.in);
+               LocalDateTime current=LocalDateTime.now();
+               System.out.println(current);
+               
+         }
+}
