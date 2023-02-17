b8e9e295b30e67fe3b320a7da2f346d1a4985ba2
Pavithra Natarajan <109599417+Pavi802@users.noreply.github.com>
2023-02-17 10:04:45 +0530

    Create vowelcount.java

---
+++ vowelcount.java
@@ -0,0 +1,19 @@
+import java.io.*;
+class cv
+{
+  public static void main(String args[])throws IOException
+  {
+    DataInputStream d=new DataInputStream(System.in);
+    System.out.println("Enter the string=");
+    String s=d.readLine();
+    int count=0;
+    for(int i=0;i<s.length();i++)
+    {
+      if(s.charAt[i]=="a"||s.charAt[i]=="e"||s.charAt[i]=="i"||s.charAt[i]=="o"||s.charAt[i]=="u")
+      {
+        count=count+1;
+      }
+    }
+   System.out.println(""+count);
+  }
+}
