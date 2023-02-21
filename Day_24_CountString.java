ad64c1c5aebf82a9b2d8b53c003c0c779a95f583
Pavithra Natarajan <109599417+Pavi802@users.noreply.github.com>
2023-02-21 10:04:13 +0530

    Create Day_24_CountString.java

---
+++ Day_24_CountString.java
@@ -0,0 +1,22 @@
+public class Main
+{
+	public static void main(String[] args) {
+		String s="Hello world";
+		int count=0;
+		for(int i=0;i<s.length();i++)
+		{
+		    if(s.charAt(i)!='\0')
+		          count++;
+		}
+		System.out.println(""+count);
+	}
+}
+
+
+
+
+
+
+
+Output:
+11
