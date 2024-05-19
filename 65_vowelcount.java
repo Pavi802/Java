5b584fc1694a99f5294924daeeced21c0ac4164c
Pavithra Natarajan <109599417+Pavi802@users.noreply.github.com>
2024-05-19 16:19:11 +0530

    Update 65_vowelcount.java

--- 65_vowelcount.java
+++ 65_vowelcount.java
@@ -9,7 +9,7 @@ class cv
     int count=0;
     for(int i=0;i<s.length();i++)
     {
-      if(s.charAt[i]=="a"||s.charAt[i]=="e"||s.charAt[i]=="i"||s.charAt[i]=="o"||s.charAt[i]=="u")
+      if(s.charAt(i)=='a'||s.charAt(i)=='e'||s.charAt(i)=='i'||s.charAt(i)=='o'||s.charAt(i)=='u')
       {
         count=count+1;
       }
