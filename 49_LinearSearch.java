b6339d893e0eb8c7d845b5836dcfdc59dfef74ad
Pavithra Natarajan <109599417+Pavi802@users.noreply.github.com>
2024-07-30 20:56:29 +0530

    Update 49_LinearSearch.java

--- 49_LinearSearch.java
+++ 49_LinearSearch.java
@@ -6,27 +6,32 @@ public class Main
 	    DataInputStream d=new DataInputStream(System.in);
 		System.out.print("Enter the limits=");
 		int i;
+		int count=0;
 		int n=Integer.parseInt(d.readLine());
 		 int[] a=new int[30];
-		 System.out.print("Enter the array elements=")
-		for(i=0;i<=n;i++)
+		 System.out.print("Enter the array elements=");
+		for(i=0;i<n;i++)
 		{
 		    a[i]=Integer.parseInt(d.readLine());
 		}
 		System.out.print("Enter the search elements=");
 		int k=Integer.parseInt(d.readLine());
-		for(int j=0;j<n;j++)
+		for(i=0;i<n;i++)
 		{
 		    if(a[i]==k)
 		    {
-		        System.out.print("Search element exists");
-		        break;
-		    }
-		    else
-		    {
-		        System.out.print("Search element not exists");
+		        //System.out.print("Search element exists");
+		        count+=1;
 		        break;
 		    }
 		}
+		if(count!=0){
+		    System.out.println("Search element exists");
+		}
+		else
+		    {
+		        System.out.print("Search element not exists");
+		        
+		    }
+		}
 	}
-}
